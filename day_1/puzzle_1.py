4350731a4eaf93f2f9632fe1f90cf941acb9efad
Ronald van den Broek <ronald.vandenbroek@live.nl>
2022-01-15 21:41:30 +0100

    Day 1 Puzzle 1

---
+++ day_1/puzzle_1.py
@@ -0,0 +1,12 @@
+depths = []
+with open('../input/day_1.txt') as f:
+    lines = f.read().splitlines()
+    depths = [int(line) for line in lines]
+
+countIncreasing = 0
+previousDepth = depths[0]
+for currentDepth in depths:
+    if currentDepth > previousDepth:
+        countIncreasing += 1
+    previousDepth = currentDepth
+print(f'Increasing Depths: {countIncreasing}')
